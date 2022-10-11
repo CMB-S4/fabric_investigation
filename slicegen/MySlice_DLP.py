ed0055eb8b8d638fe17981423d3131a6e1071ef5
dpetravick <dpetravick@yahoo.com>
2022-10-11 11:01:34 -0500

    have both nodes print histname

--- slicegen/MySlice_DLP.py
+++ slicegen/MySlice_DLP.py
@@ -13,12 +13,17 @@ slice = CfSlice(__name__)
 
 node1 = CfNode(slice, 'CMBS4Node_ncsa1', image,
                 disk=disk, cores=cores, ram=ram, site='NCSA')
+node2 = CfNode(slice, 'CMBS4Node_ncsa2', image,
+                disk=disk, cores=cores, ram=ram, site='NCSA')
+
 net1 = CfL3Network(slice, 'net1')
 
 CfNic(slice, "Node1.NIC1", node1, net1)
+CfNic(slice, "Node2.NIC1", node2, net1)
 
 
-#CFCmds(slice, "Standard Install", node1, "hostname")
+CFCmds(slice, "Standard Install", node1, "hostname")
+CFCmds(slice, "Standard Install", node2, "hostname")
 
 #
 #  Leave these function calls in place
